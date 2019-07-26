35d3543a39f175ea8fd62651f67d3c595129c0a0
Soura91 <53063590+Soura91@users.noreply.github.com>
2019-07-26 19:36:44 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -51,4 +51,24 @@ loginShell: /bin/dash
 homeDirectory: /home/student
 EOF
 
+
 ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w abcd123 -f users.ldif
+
+echo -e " 
+ldap-auth-config        ldap-auth-config/rootbindpw     password abcd123
+ldap-auth-config        ldap-auth-config/bindpw password abcd123
+ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
+ldap-auth-config        ldap-auth-config/override       boolean true
+ldap-auth-config        ldap-auth-config/pam_password   select  md5
+ldap-auth-config        ldap-auth-config/dblogin        boolean false
+slapd   slapd/allow_ldap_v2     boolean false
+libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
+ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=clemson,dc=cloudlab,dc=us
+ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
+ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
+ldap-auth-config        ldap-auth-config/rootbinddn     string  cn=admin,dc=clemson,dc=cloudlab,dc=us
+ldap-auth-config        ldap-auth-config/ldapns/ldap-server     string  ldap://192.168.1.1
+ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
+" | sudo debconf-set-selections
+
+sed 's/compat systemd/compat systemd ldap/ /etc/nsswitch.conf
