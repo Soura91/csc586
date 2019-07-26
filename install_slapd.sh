d5d3c54363562468dd7185737ccd0e573d22fb0d
Soura91 <53063590+Soura91@users.noreply.github.com>
2019-07-25 21:37:15 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -2,25 +2,25 @@
 #Install openLDAP on server
 export DEBIAN_FRONTEND='non-interactive'
 
-echo -e” slapd   slapd/internal/generated_adminpw        password test
-slapd   slapd/internal/adminpw  password test
-slapd   slapd/password2 password test
-slapd   slapd/password1 password test
-# Potentially unsafe slapd access control configuration
-slapd   slapd/unsafe_selfwrite_acl      note
-slapd   slapd/move_old_database boolean true
-slapd   slapd/no_configuration  boolean false
+echo -e” slapd   slapd/internal/generated_adminpw        password
+slapd   slapd/internal/adminpw  password
+slapd   slapd/password2 password
+slapd   slapd/password1 password
+slapd   shared/organization     string  clemson.cloudlab.us
 slapd   slapd/invalid_config    boolean true
-slapd   slapd/ppolicy_schema_needs_update       select  abort installation
-# Do you want the database to be removed when slapd is purged?
-slapd   slapd/purge_database    boolean false
-slapd   slapd/upgrade_slapcat_failure   error
-slapd   slapd/backend   select  MDB
-slapd   slapd/dump_database_destdir     string  /var/backups/slapd-VERSION
+slapd   slapd/move_old_database boolean true
 slapd   slapd/domain    string  clemson.cloudlab.us
 slapd   slapd/dump_database     select  when needed
-slapd   shared/organization     string  clemson.cloudlab.us
+slapd   slapd/dump_database_destdir     string  /var/backups/slapd-VERSION
+slapd   slapd/backend   select  MDB
+# Potentially unsafe slapd access control configuration
+slapd   slapd/unsafe_selfwrite_acl      note
+slapd   slapd/no_configuration  boolean false
+slapd   slapd/upgrade_slapcat_failure   error
 slapd   slapd/password_mismatch note
+# Do you want the database to be removed when slapd is purged?
+slapd   slapd/purge_database    boolean false
+slapd   slapd/ppolicy_schema_needs_update       select  abort installation
 " | sudo debconf-set-selections
 
 sudo apt-get update
