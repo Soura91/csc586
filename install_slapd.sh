8868f4f18b9939cd83dff37b27840426a2235c31
Soura91 <53063590+Soura91@users.noreply.github.com>
2019-07-26 18:55:24 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -29,3 +29,24 @@ slapd slapd/allow_ldap_v2 boolean false
 sudo apt-get install -y slapd ldap-utils
 #sudo dpkg-reconfigure slapd
 sudo ufw allow ldap
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W abcd123 -f basedn.ldif
+
+#echo   -n  PASS=$(slappasswd -s rammy) | awk '{print PASS}'
+PASS=$(slappasswd -s rammy)
+cat <<EOF >/local/repository/users.ldif
+dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+uid: student
+sn: Ram
+givenName: Golden
+cn: student
+displayName: student
+uidNumber: 10000
+gidNumber: 5000
+userPassword: $PASS
+gecos: Golden Ram
+loginShell: /bin/dash
+homeDirectory: /home/student
+EOF
