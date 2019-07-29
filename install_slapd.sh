ebddf7a2ec7ed2b542e165abd633cf7a4abb7aec
Soura91 <53063590+Soura91@users.noreply.github.com>
2019-07-29 15:59:42 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt update
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -27,8 +27,7 @@ slapd slapd/allow_ldap_v2 boolean false
 
 
 sudo apt-get install -y slapd ldap-utils
-sudo apt install -y debconf-utils
-sudo dpkg-reconfigure slapd
+#sudo dpkg-reconfigure slapd
 sudo ufw allow ldap
 ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w abcd123 -f basedn.ldif
 
@@ -53,4 +52,4 @@ homeDirectory: /home/student
 EOF
 
 
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w abcd123 -f users.ldif
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W abcd123 -f users.ldif
