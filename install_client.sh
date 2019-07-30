512159ab430665c2c7bf4f451896164996c9ca81
Soura91 <53063590+Soura91@users.noreply.github.com>
2019-07-30 17:19:57 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -32,7 +32,7 @@ sudo sed -i 's/systemd/systemd ldap/g' /etc/nsswitch.conf
 # Enable LDAP profile PAM
 sudo sed -i 's/\<use_authtok\>//g' /etc/pam.d/common-password
 #chmod 777 /etc/pam.d/common-password
-sudo sed -i '31 a session optional        pam_mkhomedir.so skel=/etc/skel umask=077' /etc/pam.d/common-session
+sudo sed -i '$ a session optional        pam_mkhomedir.so skel=/etc/skel umask=077' /etc/pam.d/common-session
 #chmod 777 /etc/pam.d/common-session
 
 # Authenticate student user on ldapclient
